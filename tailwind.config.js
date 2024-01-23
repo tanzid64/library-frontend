7e76071bed0141baa371ca3706bbc372621b6a79
dark <tanzid@inbox.ru>
2024-01-23 17:38:55 +0600

    Project Setup, Homepage, SignUp page, Login Modal - done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'tokumin': ["Kaisei Tokumin", 'serif'],
+      'poppins': ["Poppins", 'sans-serif']
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
