15769914939de6b34cfd089a6c7ee8b2fd179f69
dark <tanzid@inbox.ru>
2024-02-01 03:11:10 +0600

    Reg js updated

--- dist/main.js
+++ dist/main.js
@@ -1,45 +1,72 @@
 // Dark Mode
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
 // Set the default theme to dark
-document.documentElement.classList.add('dark');
-localStorage.setItem('color-theme', 'dark');
+document.documentElement.classList.add("dark");
+localStorage.setItem("color-theme", "dark");
 
 // Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
 } else {
-    themeToggleDarkIcon.classList.remove('hidden');
+  themeToggleDarkIcon.classList.remove("hidden");
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+var themeToggleBtn = document.getElementById("theme-toggle");
 
-themeToggleBtn.addEventListener('click', function() {
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
 
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
 
     // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
     } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
     }
-    
+  }
 });
+
+// Registration
+const getValue = (id) => {
+  const value = document.getElementById(id).value;
+  return value;
+};
+const handleRegistration = (event) => {
+  event.preventDefault();
+  const username = getValue("username");
+  const email = getValue("email");
+  const password = getValue("password");
+  const confirm_password = getValue("confirm_password");
+  const info = {
+    username, email, password, confirm_password,
+  };
+  console.log(info)
+
+  fetch("https://e-commerce-mzir.onrender.com/accounts/registration/", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
